0cc8c1a739fba29d10af537b3ae8d64f23f30ad1
cgome032 <cgome032@ucr.edu>
2017-05-29 16:09:18 -0700

    Enabled keypad and servo motor for unlock and lock function

--- CS120B_Project/Servo_PWM.h
+++ CS120B_Project/Servo_PWM.h
@@ -1,22 +1,22 @@
-#ifndef PWM_H_
-#define PWM_H_
+#ifndef SERVO_PWM_H_
+#define SERVO_PWM_H_
 
 #include <avr/io.h>
 // NOTE*** THIS NEW CODE TARGETS PB6 NOT PB3
 
-void set_PWM(int cycle) {
+void servo_set_PWM(int cycle) {
 	OCR1A = cycle;
-	
+
 }
 
-void PWM_on() {
+void servo_PWM_on() {
 	TCCR1A |= 1<<WGM11 | 1<<COM1A1 | 1<<COM1A0;
 	TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS10;
 }
 
-void PWM_off() {
+void servo_PWM_off() {
 	TCCR0A = 0x00;
 	TCCR0B = 0x00;
 }
 
-#endif // PWM_H_
+#endif // SERVO_PWM_H_
--- CS120B_Project/main.c
+++ CS120B_Project/main.c
@@ -6,45 +6,138 @@
  */ 
 
 #include <avr/io.h>
-#include "PWM.h"
+#include "Servo_PWM.h"
+#include "Speaker_PWM.h"
 #include "timer.h"
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "keypad.h"
 
-enum States{one,two}state;
+
+enum SM_Servo_States{one,two}servo_state;
 int pulse = 97;
-void SM_tick(){
-	switch(state){ // transitions
+void SM_Servo_tick(){
+	switch(servo_state){ // transitions
 		case one:
-			state = two;
+			servo_state = two;
 			break;
 		case two:
-			state = one;
+			servo_state = one;
 			break;
 	}
-	switch(state){ // state actions
+	switch(servo_state){ // state actions
 		case one:
-			set_PWM(1000);
+			servo_set_PWM(1000);
 			break;
 		case two:
-			set_PWM(1500);
-			//OCR1A = 1500;
+			servo_set_PWM(1500);
 			break;
 	}
 }
 
+
+enum SM_Lock_States{lock_Start,lock_Locked,lock_lockRelease,lock_Unlocked,lock_unlockRelease}lock_state;
+char passCode[6] = {'#','1','2','3','4','#'};
+unsigned char lockCounter = 0;
+void SM_Lock_Tick(){
+	unsigned char x = GetKeypadKey();
+	switch(lock_state){		// Transitions
+		case lock_Start:
+			lockCounter = 0;
+			lock_state = lock_Locked;
+			servo_set_PWM(1500);
+			break;
+		case lock_Locked:
+			if(x == '\0'){
+				lock_state = lock_Locked;
+			}
+			else{
+				lock_state = lock_lockRelease;
+				if(x==passCode[lockCounter]){
+					lockCounter++;
+				}
+				else{
+					lockCounter = 0;
+				}
+			}
+			break;
+		case lock_lockRelease:
+			if(x != '\0'){
+				lock_state = lock_lockRelease;
+			}
+			else if(x == '\0' && lockCounter<=5){
+				lock_state = lock_Locked;
+			}
+			else if(x == '\0' && lockCounter > 5){
+				servo_set_PWM(1000);
+				lock_state = lock_Unlocked;
+				lockCounter = 0;
+			}
+			break;
+		case lock_Unlocked:
+			if(x == '\0'){
+				lock_state = lock_Unlocked;
+			}
+			else{
+				lock_state = lock_unlockRelease;
+				if(x==passCode[lockCounter]){
+					lockCounter++;
+				}
+				else{
+					lockCounter = 0;
+				}
+			}
+			break;
+		case lock_unlockRelease:
+			if(x != '\0'){
+				lock_state = lock_unlockRelease;
+			}
+			else if(x == '\0' && lockCounter<=5){
+				lock_state = lock_Unlocked;
+			}
+			else if(x == '\0' && lockCounter > 5){
+				servo_set_PWM(1500);
+				lock_state = lock_Locked;
+				lockCounter = 0;
+			}
+			break;
+	}
+	switch(lock_state){		// State actions
+		case lock_Start: 
+			break;
+		case lock_Locked:
+			speaker_set_PWM(0);
+			break;
+		case lock_lockRelease:
+			break;
+		case lock_Unlocked:
+			speaker_set_PWM(329.63);
+			break;
+		case lock_unlockRelease:
+			break;
+	}
+}
+
+
+
 int main(void)
 {
+
+	DDRB = 0xFF; PORTB = 0xFF; // Set B to output
+	DDRC = 0xF0; PORTC = 0x0F; // Set PC7 to input keypad
 	DDRD |= 0xFF;
 
-	PWM_on();
+	servo_PWM_on();
+	speaker_PWM_on();
 	TimerOn();
-	TimerSet(100);
-	state = one;
+	TimerSet(1);
+	servo_state = one;
+	lock_state = lock_Start;
 
 	while (1)
 	{
-		SM_tick();
+		//SM_Servo_tick();
+		SM_Lock_Tick();
 		while(!TimerFlag){}
 		TimerFlag=0;
 	}
