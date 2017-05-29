6d73e5ccb54fe38c089bc7610ac003a31c736d1c
cgome032 <cgome032@ucr.edu>
2017-05-28 20:58:04 -0700

    Initial Add

---
+++ CS120B_Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * CS120B_Project.c
+ *
+ * Created: 5/28/2017 8:40:42 PM
+ * Author : Carlos
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
